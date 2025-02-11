ca505d7d80e64e6e1a9892a90fb5382a3cefde44
Leslie Williamson <leslie.williamson@live.ca>
2025-02-11 15:36:49 -0500

    Error checking for new article creation. Display error messages on failure and flash message on success.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,20 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new # make sure we have an instance on initial page load
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    # redirect_to article_path(@article)
-    redirect_to @article # shorthand for the above line
+
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      # redirect_to article_path(@article)
+      redirect_to @article # shorthand for the above line
+    else
+      # need to add status: :unprocessable_entity in Rails 8
+      render :new, status: :unprocessable_entity
+      # you can also use a symbol instead of a string. i.e :new instead of "new"
+    end
   end
 end
