798014fcab3f9ba6046846833c7318616c11c99a
Leslie Williamson <leslie.williamson@live.ca>
2025-02-12 11:56:46 -0500

    Added edit and update routes and controller actions. Added edit view.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def show
     # binding.break
+    # need exception handling for RecordNotFound
     @article = Article.find(params[:id])
   end
 
@@ -10,6 +11,11 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new # make sure we have an instance on initial page load
+  end
+
+  def edit
+    # need exception handling for RecordNotFound
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -23,6 +29,18 @@ class ArticlesController < ApplicationController
       # need to add status: :unprocessable_entity in Rails 8
       render :new, status: :unprocessable_entity
       # you can also use a symbol instead of a string. i.e :new instead of "new"
+    end
+  end
+
+
+  def update
+    # need exception handling for RecordNotFound
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article updated successfully."
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 end
