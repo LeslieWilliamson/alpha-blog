2d8221102c2b91082dc0f0dd7a4ab5212c368b13
Leslie Williamson <leslie.williamson@live.ca>
2025-02-26 09:37:05 -0500

    Add create and destroy actions for SessionController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully."
+      redirect_to user
+    else
+      flash.now[:alert] = "Authentication failed"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You are logged out."
+    redirect_to root_path
   end
 end
