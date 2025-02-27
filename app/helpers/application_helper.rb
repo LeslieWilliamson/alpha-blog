2496b28307dbd0cb0018be1f2f528d7f9bd17bde
Leslie Williamson <leslie.williamson@live.ca>
2025-02-27 12:34:06 -0500

    Add margins to gravatar so User show card isn't too narrow when edit/delete buttons are not shown.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
     hash = Digest::MD5.hexdigest(email_address)
     size = options[:size]
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
-    image_tag gravatar_url, class: "rounded shadow mx-auto d-block"
+    image_tag gravatar_url, class: "rounded shadow mx-auto d-block mt-3 mb-3"
   end
 end
