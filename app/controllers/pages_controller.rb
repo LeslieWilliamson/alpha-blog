576fd0b8024566d9b5e5e3076d6d70a15b531103
Leslie Williamson <leslie.williamson@live.ca>
2025-02-27 11:13:13 -0500

    Move helper functions to ApplicationControler and declare them as helper methods.  Update create action to associate new article with logged in user. Redirect root action to user show page when user is logged in.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def home
-    
+    # The redirect below is an alternative solution to the code in views/layouts/_navigation.html.erb
+    # redirect_to user_path(current_user) if logged_in?
   end
 
   def about
-    
   end
 end
