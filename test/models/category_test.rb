4ee4dae424f586b25ab5e42793ff4de9cbb5eb07
Leslie Williamson <leslie.williamson@live.ca>
2025-03-05 12:06:16 -0500

    Add Category validations and unit tests for validations

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,8 +1,32 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  test "category should be valid" do
+  def setup
     @category = Category.new(name: "Sports")
+  end
+
+
+  test "category should be valid" do
     assert @category.valid?
+  end
+
+  test "category name is required" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+  test "category name should be unique" do
+    @category.save
+    @category2 = Category.new(name: "Sports")
+    assert_not @category2.valid?
+  end
+
+  test "category name length should be 25 character maximum" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "category name length should be 3 characters minimum" do
+    @category.name = "aa"
+    assert_not @category.valid?
   end
 end
